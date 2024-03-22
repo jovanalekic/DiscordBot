3ba3c64ff71a477fea113b88261814db81268ff7
DanQZ <danqzbusiness@gmail.com>
2024-03-21 20:38:13 -0400

    can make a txt of all page titles from the source code of the wiki page of all aesthetnics

--- test1.py
+++ test1.py
@@ -1,3 +1,5 @@
+
+
 print(5)
 print("ASDFASDF")
 
@@ -6,3 +8,55 @@ print("PISS")
 print("rawr")
 
 print("sdfsf")
+
+#creates and clears output file
+outputFileName = "pageNames.txt"
+outputFile = open(outputFileName, "w")
+outputFile.write("")
+outputFile.close()
+
+outputFile = open(outputFileName, "a")
+
+sourceFileName = "wikiSource.txt"
+sourceFile = open(sourceFileName, 'r', encoding='utf8', errors='ignore')
+
+contents = sourceFile.read()
+contentsByLine = contents.split("\n")
+
+pageArray = []
+
+prefix = "title="
+
+def returnThemeOfLine(line):
+  x = 0
+  while x < len(line) - (len(prefix)):
+    if line[x:x+len(prefix)] == prefix:
+      i = x + len(prefix) + 1
+      while line[i:i+1] != "\"":
+        i += 1
+      return line[x + len(prefix)+1:i]
+      x = i
+    x += 1
+  return "NO THEME"
+    
+for line in contentsByLine:
+  
+  if(len(line) > 150):
+    continue
+  
+  theme = returnThemeOfLine(line)
+  if theme == "NO THEME":
+    continue
+  
+  pageArray.append(theme)
+  
+
+for k in pageArray:
+  try:
+    outputFile.write(k + "\n")
+  except UnicodeEncodeError:
+    print("could not write \"" + k +"\"")
+    continue
+    
+sourceFile.close()
+outputFile.close()
